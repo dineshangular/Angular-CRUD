f9be29a817d3c20c5c6309d97047ee727f1cb9f7
dineshangular <dineshkarki666@outlook.com>
2018-10-11 00:30:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-CRUD';
+  title = 'Angular 5 CRUD operation with an array';
+  employees=[
+    {name:"Dinesh", position:"UI Developer"},
+    {name:"Nandhini", position:"Oracle Developer"},
+    {name:"Mukesh", position:"Business"}
+  ];
+  model:any={};
+  model2:any={};
+  msg:any="";
+  addEmployee(){
+    this.employees.push(this.model);
+    this.msg="Record is successfully added...";
+  }
+  deleteEmployee(i) {
+    this.employees.splice(i,1);
+    this.msg="Record is successfully deleted..."
+  }
+  myValue;
+  editEmployee(k){
+    this.model2.name=this.employees[k].name;
+    this.model2.position=this.employees[k].position;
+    this.myValue=k;
+
+  }
+  updateEmployee(){
+    let k = this.myValue;
+    for(let i=0; i<this.employees.length; i++){
+      if(i==k){
+        this.employees[i]=this.model2;
+        this.model2={};
+        this.msg="Record is successfully updated..."
+      }
+    }
+  }
+  clickMe(){
+    this.msg="";
+  }
+
 }
